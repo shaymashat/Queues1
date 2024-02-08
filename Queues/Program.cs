a1129b3d34e495156a05f8afc45c03603938897c
User <User@LAB3-3>
2024-02-08 14:34:59 +0200

    Add project files.

---
+++ Queues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Queues.Models;
+namespace Queues
+{
+    internal class Program
+    {
+        
+     
+        static void Main(string[] args)
+        {
+            Queue<int> q1= new Queue<int>();    
+            q1.Insert(1);
+            q1.Insert(2);
+            q1.Insert(3);
+            q1.Insert(4);
+            q1.Insert(5);
+            Console.WriteLine(q1);
+            Console.WriteLine(QueueHelper.Count(q1));
+        }
+    }
+}
